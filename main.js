28d7715f86b9840ad91517044ec43947a12bf393
mattrudin <matthias.rudin@live.com>
2018-06-28 20:18:53 +0200

    Final version

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ minMax([2334454,5])   == [5, 2334454]
 minMax([1])           == [1, 1]
 */
 
-function minMax(arr){
-  return [0,0]; // fix me!
+const minMax = (arr) => {
+	const array = [];
+	array.push(Math.min(...arr));
+	array.push(Math.max(...arr));
+	return array;
 }
+
+
+//Tests
+minMax([3,5,1,7,6]);
